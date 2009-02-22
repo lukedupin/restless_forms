c86153b2084f3f28d9dd0ccfdfbb8c0b0a4b07de
Rhino <orbital.sfear@gmail.com>
2009-02-22 09:23:15 -0800

    Trying to add in my install script and move the forms

--- install.rb
+++ install.rb
@@ -1,1 +1,22 @@
 # Install hook code here
+def erb(t)
+  ERB.new( File.open(t).read, 0, '<>' ).result
+end
+
+#Start building out models
+puts "#{'Moving Modules Begin'.ljust(40,'-')}"
+
+  #Create my instance variable to keep record of what I've done
+@models = Hash.new
+path = "#{File.dirname(__FILE__)}/../user_modules"
+
+  #copy my modules
+`ls #{path}`.split(/\n/).each do |f|
+  puts "Copying #{f}"
+  file = File.open("#{File.dirname(__FILE__)}/../../../../lib/#{f}", 'w')
+  file.puts erb("#{path}/#{f}")
+  file.close
+end
+
+puts "#{'Modules Finished'.ljust(40,'-')}"
+puts
